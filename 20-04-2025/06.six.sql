0f5fba2267ef49f19d10e786d6ef8775fba3c84c
Shiva Sunchu <shivasunchu06@gmail.com>
2025-04-21 12:03:40 +0530

    updating 20-04-2025

--- 20-04-2025/06.six.sql
+++ 20-04-2025/06.six.sql
@@ -16,3 +16,38 @@
 -- checkin_date < checkout_date
 
 -- No room can be double-booked between dates (challenge: can be handled in advanced constraints/triggers later)
+
+CREATE TABLE hotels (
+    hotel_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    hotel_name VARCHAR(100) NOT NULL UNIQUE CHECK (LENGTH(hotel_name) >= 5),
+    hotel_location VARCHAR(100) NOT NULL CHECK (LENGTH(hotel_location) >= 5),
+    rating SMALLINT DEFAULT 1 CHECK (rating BETWEEN 1 AND 5)
+);
+
+CREATE TYPE room_type_enum as ENUM('Standard', 'Deluxe', 'Suite');
+
+CREATE TABLE rooms (
+    room_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    hotel_id INTEGER NOT NULL REFERENCES hotels(hotel_id),
+    room_type room_type_enum NOT NULL,
+    price_per_night NUMERIC(10, 2) NOT NULL CHECK (price_per_night > 0)
+);
+
+CREATE TABLE customers (
+    customer_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    full_name VARCHAR(100) NOT NULL CHECK (LENGTH(full_name) >= 3),
+    email VARCHAR(35) NOT NULL UNIQUE CHECK (POSITION('@' IN email) > 0),
+    phone CHAR(10) NOT NULL UNIQUE CHECK (
+        phone ~ '^[6-9][0-9]{9}$'
+    )
+);
+
+CREATE TABLE bookings (
+    booking_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    customer_id INTEGER NOT NULL REFERENCES customers(customer_id),
+    room_id INTEGER NOT NULL REFERENCES rooms(room_id),
+    checkin_date DATE NOT NULL,
+    checkout_date DATE NOT NULL CHECK (
+        checkout_date::DATE > checkin_date::DATE
+    )
+)
--- 20-04-2025/07.seven.sql
+++ 20-04-2025/07.seven.sql
@@ -23,3 +23,56 @@
 
 -- price_at_order_time stores product price at time of order (so price changes don’t affect past orders).
 
+
+CREATE TABLE categories (
+    category_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    category_name VARCHAR(20) UNIQUE NOT NULL CHECK (LENGTH(category_name) >= 3)
+);
+
+CREATE TABLE products (
+    product_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    product_name VARCHAR(70) NOT NULL UNIQUE CHECK (LENGTH(product_name) >= 3),
+    category_id INTEGER REFERENCES categories(category_id) ON DELETE SET NULL,
+    price NUMERIC(10,2) NOT NULL CHECK (price > 0),
+    stock_quantity SMALLINT NOT NULL DEFAULT 0
+);
+
+CREATE TABLE users (
+    user_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    username VARCHAR(25) NOT NULL UNIQUE CHECK (LENGTH(username) >= 3),
+    email VARCHAR(30) NOT NULL UNIQUE CHECK (POSITION('@' IN email) > 0),
+    phone VARCHAR(10) NOT NULL UNIQUE CHECK (
+        phone ~ '^[6-9][0-9]{9}$'
+    )
+);
+
+CREATE TABLE carts (
+    cart_id INTEGER GENERATED ALWAYS AS IDENTITY,
+    user_id INTEGER NOT NULL REFERENCES users(user_id),
+    created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (cart_id)
+);
+
+CREATE TABLE cart_items (
+    cart_id INTEGER NOT NULL REFERENCES carts(cart_id),
+    product_id  INTEGER NOT NULL REFERENCES products(product_id),
+    quantity SMALLINT NOT NULL CHECK (quantity BETWEEN 1 AND 100),
+    PRIMARY KEY (cart_id, product_id)
+);
+
+CREATE TYPE orders_enum AS ENUM('Pending', 'Shipped', 'Delivered');
+
+CREATE TABLE orders (
+    order_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    user_id INTEGER NOT NULL REFERENCES users(user_id),
+    order_date DATE NOT NULL DEFAULT CURRENT_DATE,
+    order_status orders_enum NOT NULL
+);
+
+CREATE TABLE order_items (
+    order_id INTEGER NOT NULL REFERENCES orders(order_id),
+    product_id INTEGER NOT NULL REFERENCES products(product_id),
+    quantity SMALLINT NOT NULL CHECK (quantity BETWEEN 1 AND 100),
+    price_at_order_time NUMERIC(10,2) NOT NULL CHECK (price_at_order_time > 0),
+    PRIMARY KEY (order_id, product_id)
+);
